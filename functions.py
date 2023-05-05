66b6028080cf4ec9625d51b679718cf51b7f968e
oguzhan2027 <oguzhancelik2027@gmail.com>
2023-05-05 22:57:56 +0300

    komut

---
+++ functions.py
@@ -0,0 +1,5 @@
+from random import randint as r
+class Game:
+    @staticmethod
+    def roll_dice():
+        return r(1, 7)
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import discord
 from discord.ext import commands
+from utils import *
+from functions import *
 
-Bot = commands.Bot(command_prefix="!oc", intents = discord.Intents.all())
-TOKEN = open("token.txt","r").read()
+Bot = commands.Bot(command_prefix="!ock", intents = discord.Intents.all())
+game = Game()
 @Bot.event
 async def on_ready():
     print("ben hazırım")
@@ -18,8 +20,11 @@ async def on_member_remove(member):
     channel = discord.utils.get(member.guild.text_channels, name="gülegüle")
     await channel.send(f"{member} aramızdan ayrıldı :(")
     print(f"{member} aramızdan ayrıldı :(")
-@Bot.command()
-async def oguzhancelik(msg):
-    await msg.send('en iyisi')
+@Bot.command(aliases = ["game", "oyun"])
+async def ock(ctx, *args):
+    if "roll" in args:
+        await ctx.send(game.roll_dice())
+    else:
+        await ctx.send('en iyisi')
 
 Bot.run(TOKEN)
